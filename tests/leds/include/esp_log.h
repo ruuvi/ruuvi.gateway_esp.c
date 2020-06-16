597bef6e10a25ac5c997146c010cc97af4c79d72
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-06-16 08:18:24 +0700

    Unit-tests for leds.c module

---
+++ tests/leds/include/esp_log.h
@@ -0,0 +1,28 @@
+#ifndef ESP_LOG_H
+#define ESP_LOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Log level
+ *
+ */
+typedef enum {
+    ESP_LOG_NONE,       /*!< No log output */
+    ESP_LOG_ERROR,      /*!< Critical errors, software module can not recover on its own */
+    ESP_LOG_WARN,       /*!< Error conditions from which recovery measures have been taken */
+    ESP_LOG_INFO,       /*!< Information messages which describe normal flow of events */
+    ESP_LOG_DEBUG,      /*!< Extra information which is not necessary for normal use (values, pointers, sizes, etc). */
+    ESP_LOG_VERBOSE     /*!< Bigger chunks of debugging information, or frequent messages which can potentially flood the output. */
+} esp_log_level_t;
+
+#define ESP_LOGI(tag, fmt, ...)
+#define ESP_LOGE(tag, fmt, ...)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ESP_LOG_H
