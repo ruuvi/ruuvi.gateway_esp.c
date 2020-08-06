3dabac2ae969e975db472350c1864705892d336e
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-08-06 10:28:16 +0700

    #63: scripts/conv_gtestresults_to_sonarcloud.py: remove python 3.8 features, make script compatible with python 3.5

--- scripts/conv_gtestresults_to_sonarcloud.py
+++ scripts/conv_gtestresults_to_sonarcloud.py
@@ -5,22 +5,25 @@ import re
 import xml.etree.ElementTree as xml_et
 from xml.dom import minidom
 
+assert sys.version_info >= (3, 5)
+
 
 def scan_cpp(base_path, rel_project_path):
     test_project_path = os.path.join(base_path, rel_project_path)
     if not os.path.isdir(test_project_path):
-        print(f'Error: there is no folder: {test_project_path}')
+        print('Error: there is no folder: %s' % test_project_path)
         sys.exit(1)
     dict_test_suites = dict()
-    re_line = re.compile('^class\s+([\w\d_]+)\s*:\s*public\s*::testing::Test')
+    re_line = re.compile(r'^class\s+([\w\d_]+)\s*:\s*public\s*::testing::Test')
     for dirpath, dirnames, filenames in os.walk(test_project_path):
         for file in filenames:
             if os.path.splitext(file)[1] == '.cpp':
                 cpp_file_path = os.path.join(dirpath, file)
-                print(f'    Found: {os.path.relpath(cpp_file_path)}')
+                print('    Found: %s' % os.path.relpath(cpp_file_path))
                 with open(cpp_file_path) as fd:
                     for line in fd.readlines():
-                        if m := re_line.match(line):
+                        m = re_line.match(line)
+                        if m:
                             test_suit_name = m.group(1)
                             rel_path = os.path.relpath(cpp_file_path, base_path)
                             dict_test_suites[test_suit_name] = rel_path
@@ -34,16 +37,16 @@ def handle_gtest_results(input_file, base_path, rel_project_path, xml_root):
         input_content = fd.read()
     gtest_xml = xml_et.XML(input_content)
     if gtest_xml.tag != 'testsuites':
-        print(f'Error: Bad input file: expected "testsuites" section, but found "{gtest_xml.tag}"')
+        print('Error: Bad input file: expected "testsuites" section, but found "%s"' % gtest_xml.tag)
         sys.exit(1)
     test_suits = list(gtest_xml)
     for test_suite in test_suits:
         assert isinstance(test_suite, xml_et.Element)
         if test_suite.tag != 'testsuite':
-            print(f'Error: Bad input file: expected "testsuite" section, but found "{test_suite.tag}"')
+            print('Error: Bad input file: expected "testsuite" section, but found "%s"' % test_suite.tag)
             sys.exit(1)
         test_suite_name = test_suite.attrib['name']
-        print(f'    Test suite name: {test_suite_name}')
+        print('    Test suite name: %s' % test_suite_name)
         test_suite_cpp = dict_test_suites[test_suite_name]
         xml_elem_file = xml_et.Element('file')
         xml_elem_file.set('path', test_suite_cpp)
@@ -51,7 +54,7 @@ def handle_gtest_results(input_file, base_path, rel_project_path, xml_root):
         for test_case in test_cases:
             assert isinstance(test_case, xml_et.Element)
             if test_case.tag != 'testcase':
-                print(f'Error: Bad input file: expected "testcase" section, but found "{test_case.tag}"')
+                print('Error: Bad input file: expected "testcase" section, but found "%s"' % test_case.tag)
                 sys.exit(1)
             test_case_name = test_case.attrib['name']
             test_case_time = test_case.attrib['time']
@@ -59,7 +62,7 @@ def handle_gtest_results(input_file, base_path, rel_project_path, xml_root):
             test_case_status = test_case.attrib['status']
             test_case_result = test_case.attrib['result']
             xml_elem_test_case = xml_et.Element('testCase')
-            print(f'        Test case: {test_case_name}: {test_case_status}: {test_case_result}')
+            print('        Test case: %s: %s: %s' % (test_case_name, test_case_status, test_case_result))
             xml_elem_test_case.set('name', test_case_name)
             xml_elem_test_case.set('duration', test_case_time)
             if test_case_status == 'run':
@@ -68,19 +71,19 @@ def handle_gtest_results(input_file, base_path, rel_project_path, xml_root):
                     for failure in failures:
                         assert isinstance(failure, xml_et.Element)
                         if failure.tag != 'failure':
-                            print(f'Error: Bad input file: expected "failure" section, but found "{failure.tag}"')
+                            print('Error: Bad input file: expected "failure" section, but found "%s"' % failure.tag)
                             sys.exit(1)
                         failure_message = failure.attrib['message']
                         failure_text = failure.text
                         failure_message_short = failure_message.partition('\n')[0]
-                        print(f'            Failure: {failure_message_short}')
+                        print('            Failure: %s' % failure_message_short)
                         xml_elem_failure = xml_et.Element('failure')
                         # failure_message = failure_message.replace('\n', '&#x0A')
                         xml_elem_failure.set('message', failure_message)
                         xml_elem_test_case.append(xml_elem_failure)
                     pass
                 else:
-                    print(f'Error: Unknown test case result in status "run": {test_case_result}')
+                    print('Error: Unknown test case result in status "run": %s' % test_case_result)
                     sys.exit(1)
             elif test_case_status == 'notrun':
                 if test_case_result == 'suppressed':
@@ -89,10 +92,10 @@ def handle_gtest_results(input_file, base_path, rel_project_path, xml_root):
                     xml_elem_skipped.set('message', 'disabled')
                     xml_elem_test_case.append(xml_elem_skipped)
                 else:
-                    print(f'Error: Unknown test case result in status "notrun": {test_case_result}')
+                    print('Error: Unknown test case result in status "notrun": %s' % test_case_result)
                     sys.exit(1)
             else:
-                print(f'Error: Unknown test case status: {test_case_status}')
+                print('Error: Unknown test case status: %s' % test_case_status)
                 sys.exit(1)
             xml_elem_file.append(xml_elem_test_case)
             pass
@@ -127,7 +130,7 @@ def main():
                 project_path = os.path.join(tests_path, project_name)
                 rel_project_path = os.path.relpath(project_path, base_path)
                 list_of_test_projects.append(rel_project_path)
-                print(f'Found google-test results: {os.path.join(dirpath, file)}')
+                print('Found google-test results: %s' % os.path.join(dirpath, file))
                 handle_gtest_results(os.path.join(dirpath, file), base_path, rel_project_path, xml_root)
 
     xml_str = minidom.parseString(xml_et.tostring(xml_root)).toprettyxml(indent="   ")
