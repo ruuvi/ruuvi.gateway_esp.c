64a2368c9b69abf72e81b3893b369bcab0351b93
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-10-14 21:00:58 +0700

    #59: Move SWD GPIO and NRF52 NVMC defines from nrf52swd.c to ruuvi.boards.c submodule

--- main/nrf52swd.c
+++ main/nrf52swd.c
@@ -13,35 +13,13 @@
 #include <driver/gpio.h>
 #include "libswd.h"
 #include "esp_log.h"
+#include "ruuvi_board_gwesp.h"
 
 static const char *TAG = "SWD";
 
 typedef int LibSWD_ReturnCode_t;
 typedef int LibSWD_IdCode_t;
 typedef int LibSWD_Data_t;
-
-#define NRF52_NVMC_BASE                    0x4001E000UL
-#define NRF52_NVMC_REG_READY               (NRF52_NVMC_BASE + 0x400U)
-#define NRF52_NVMC_REG_CONFIG              (NRF52_NVMC_BASE + 0x504U)
-#define NRF52_NVMC_REG_ERASEPAGE           (NRF52_NVMC_BASE + 0x508U)
-#define NRF52_NVMC_REG_ERASEALL            (NRF52_NVMC_BASE + 0x50CU)
-#define NRF52_NVMC_REG_ERASEUICR           (NRF52_NVMC_BASE + 0x514U)
-#define NRF52_NVMC_REG_ERASEPAGEPARTIAL    (NRF52_NVMC_BASE + 0x518U)
-#define NRF52_NVMC_REG_ERASEPAGEPARTIALCFG (NRF52_NVMC_BASE + 0x51CU)
-
-#define NRF52_NVMC_REG_READY__MASK (0x00000001UL)
-
-#define NRF52_NVMC_REG_CONFIG__WEN_MASK (0x03UL)
-#define NRF52_NVMC_REG_CONFIG__WEN_REN  (0U)
-#define NRF52_NVMC_REG_CONFIG__WEN_WEN  (1U)
-#define NRF52_NVMC_REG_CONFIG__WEN_EEN  (2U)
-
-#define NRF52_NVMC_REG_ERASEALL__NO_OP (0U)
-#define NRF52_NVMC_REG_ERASEALL__ERASE (1U)
-
-#define NRF52_GPIO_SWD_CLK GPIO_NUM_15
-#define NRF52_GPIO_SWD_IO  GPIO_NUM_16
-#define NRF52_GPIO_NRST    GPIO_NUM_17
 
 #define NRF52SWD_LOG_ERR(func, err) \
     do \
