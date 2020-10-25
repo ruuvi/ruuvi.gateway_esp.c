5f5794ca9b4ffd65c54ed9ba95b7a00d512a38d6
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-10-26 02:15:40 +0700

    #101: time.c: refactoring, fix various code smells

--- main/time.c
+++ main/time.c
@@ -5,7 +5,7 @@
  * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
  */
 
-#include "esp_log.h"
+#include "time_task.h"
 #include "esp_sntp.h"
 #include "esp_system.h"
 #include "freertos/FreeRTOS.h"
@@ -13,32 +13,32 @@
 #include "freertos/task.h"
 #include "lwip/err.h"
 #include "sntp.h"
-#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/time.h>
 #include <time.h>
 #include "attribs.h"
 #include "time_units.h"
+#include "log.h"
 
-#define TIME_SYNC_BIT (1 << 0)
+#define TIME_SYNC_BIT (1U << 0U)
 
 #define TM_YEAR_BASE (1900)
 #define TM_YEAR_MIN  (2020)
 
-static TaskHandle_t task_time        = NULL;
-EventGroupHandle_t  time_event_group = NULL;
-EventBits_t         uxBits;
-static const char   TAG[] = "time";
+static TaskHandle_t       gh_time_task        = NULL;
+static EventGroupHandle_t gh_time_event_group = NULL;
+EventBits_t               uxBits;
+
+static const char TAG[] = "time";
 
 static time_t
 wait_for_sntp(void)
 {
     // wait for time to be set
-    time_t    now         = 0;
-    struct tm timeinfo    = { 0 };
-    int       retry       = 0;
-    const int retry_count = 20;
+    time_t        now         = 0;
+    struct tm     timeinfo    = { 0 };
+    int32_t       retry       = 0;
+    const int32_t retry_count = 20;
 
     while (timeinfo.tm_year < (TM_YEAR_MIN - TM_YEAR_BASE))
     {
@@ -47,7 +47,7 @@ wait_for_sntp(void)
         {
             break;
         }
-        ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        LOG_INFO("Waiting for system time to be set... (%d/%d)", retry, retry_count);
         const uint32_t delay_ms = 3000;
         vTaskDelay(pdMS_TO_TICKS(delay_ms));
         time(&now);
@@ -67,7 +67,7 @@ wait_for_sntp(void)
 static void
 sync_sntp(void)
 {
-    ESP_LOGI(TAG, "Synchronizing SNTP");
+    LOG_INFO("Synchronizing SNTP");
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
     sntp_set_sync_mode(SNTP_SYNC_MODE_SMOOTH);
     u8_t server_idx = 0;
@@ -79,44 +79,44 @@ sync_sntp(void)
     server_idx += 1;
     sntp_setservername(server_idx, "pool.ntp.org");
     sntp_init();
-    time_t now = wait_for_sntp();
+    const time_t now = wait_for_sntp();
     sntp_stop();
 
-    if (now)
+    if (0 != now)
     {
         char      strftime_buf[64];
         struct tm timeinfo = { 0 };
         localtime_r(&now, &timeinfo);
         strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-        ESP_LOGI(TAG, "SNTP Synchronized: %s", strftime_buf);
+        LOG_INFO("SNTP Synchronized: %s", strftime_buf);
     }
     else
     {
-        ESP_LOGW(TAG, "SNTP not synchronized");
+        LOG_WARN("SNTP not synchronized");
     }
 }
 
 void
 time_sync(void)
 {
-    xEventGroupSetBits(time_event_group, TIME_SYNC_BIT);
+    xEventGroupSetBits(gh_time_event_group, TIME_SYNC_BIT);
 }
 
 ATTR_NORETURN
 static void
-time_task(void *param)
+time_task(void *p_param)
 {
-    (void)param;
-    if (!time_event_group)
+    (void)p_param;
+    if (NULL == gh_time_event_group)
     {
-        time_event_group = xEventGroupCreate();
+        gh_time_event_group = xEventGroupCreate();
     }
 
     for (;;)
     {
         // Wait for trigger command or 24 hours to synch time.
         xEventGroupWaitBits(
-            time_event_group,
+            gh_time_event_group,
             TIME_SYNC_BIT,
             pdTRUE,
             pdFALSE,
@@ -130,8 +130,9 @@ time_task(void *param)
 void
 time_init(void)
 {
-    const uint32_t stack_size = 3 * 1024;
-    xTaskCreate(&time_task, "time_task", stack_size, NULL, 1, &task_time);
+    const uint32_t    stack_size    = 3U * 1024U;
+    const UBaseType_t task_priority = 1;
+    xTaskCreate(&time_task, "time_task", stack_size, NULL, task_priority, &gh_time_task);
 }
 
 void
@@ -139,10 +140,10 @@ time_stop(void)
 {
     sntp_stop();
 
-    if (task_time)
+    if (NULL != gh_time_task)
     {
-        vTaskDelete(task_time);
-        vEventGroupDelete(time_event_group);
-        time_event_group = NULL;
+        vTaskDelete(gh_time_task);
+        vEventGroupDelete(gh_time_event_group);
+        gh_time_event_group = NULL;
     }
 }
