7b5b569c15961b0f582b25fccdd74924411381a9
Alexey Skvortsov <alexeyn.skvortsov@gmail.com>
2020-11-04 02:22:47 +0700

    #101: metrics.c: Fix code smell: Refactor structure so it has no more than 20 fields

--- main/metrics.c
+++ main/metrics.c
@@ -31,71 +31,81 @@ get_total_free_bytes(const uint32_t caps)
 
 typedef unsigned long ulong_t;
 
+typedef struct metrics_total_free_info_t
+{
+    ulong_t size_exec;
+    ulong_t size_32bit;
+    ulong_t size_8bit;
+    ulong_t size_dma;
+    ulong_t size_pid2;
+    ulong_t size_pid3;
+    ulong_t size_pid4;
+    ulong_t size_pid5;
+    ulong_t size_pid6;
+    ulong_t size_pid7;
+    ulong_t size_spiram;
+    ulong_t size_internal;
+    ulong_t size_default;
+} metrics_total_free_info_t;
+
+typedef struct metrics_largest_free_info_t
+{
+    ulong_t size_exec;
+    ulong_t size_32bit;
+    ulong_t size_8bit;
+    ulong_t size_dma;
+    ulong_t size_pid2;
+    ulong_t size_pid3;
+    ulong_t size_pid4;
+    ulong_t size_pid5;
+    ulong_t size_pid6;
+    ulong_t size_pid7;
+    ulong_t size_spiram;
+    ulong_t size_internal;
+    ulong_t size_default;
+} metrics_largest_free_info_t;
+
 typedef struct metrics_info_t
 {
-    uint64_t received_advertisements;
-    int64_t  uptime_us;
-    ulong_t  total_free_bytes_exec;
-    ulong_t  total_free_bytes_32bit;
-    ulong_t  total_free_bytes_8bit;
-    ulong_t  total_free_bytes_dma;
-    ulong_t  total_free_bytes_pid2;
-    ulong_t  total_free_bytes_pid3;
-    ulong_t  total_free_bytes_pid4;
-    ulong_t  total_free_bytes_pid5;
-    ulong_t  total_free_bytes_pid6;
-    ulong_t  total_free_bytes_pid7;
-    ulong_t  total_free_bytes_spiram;
-    ulong_t  total_free_bytes_internal;
-    ulong_t  total_free_bytes_default;
+    uint64_t                    received_advertisements;
+    int64_t                     uptime_us;
+    metrics_total_free_info_t   total_free_bytes;
+    metrics_largest_free_info_t largest_free_block;
 
-    ulong_t largest_free_block_exec;
-    ulong_t largest_free_block_32bit;
-    ulong_t largest_free_block_8bit;
-    ulong_t largest_free_block_dma;
-    ulong_t largest_free_block_pid2;
-    ulong_t largest_free_block_pid3;
-    ulong_t largest_free_block_pid4;
-    ulong_t largest_free_block_pid5;
-    ulong_t largest_free_block_pid6;
-    ulong_t largest_free_block_pid7;
-    ulong_t largest_free_block_spiram;
-    ulong_t largest_free_block_internal;
-    ulong_t largest_free_block_default;
 } metrics_info_t;
 
 static metrics_info_t
 gen_metrics(void)
 {
     const metrics_info_t metrics = {
-        .received_advertisements     = gw_metrics.received_advertisements,
-        .uptime_us                   = esp_timer_get_time(),
-        .total_free_bytes_exec       = (ulong_t)get_total_free_bytes(MALLOC_CAP_EXEC),
-        .total_free_bytes_32bit      = (ulong_t)get_total_free_bytes(MALLOC_CAP_32BIT),
-        .total_free_bytes_8bit       = (ulong_t)get_total_free_bytes(MALLOC_CAP_8BIT),
-        .total_free_bytes_dma        = (ulong_t)get_total_free_bytes(MALLOC_CAP_DMA),
-        .total_free_bytes_pid2       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID2),
-        .total_free_bytes_pid3       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID3),
-        .total_free_bytes_pid4       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID4),
-        .total_free_bytes_pid5       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID5),
-        .total_free_bytes_pid6       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID6),
-        .total_free_bytes_pid7       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID7),
-        .total_free_bytes_spiram     = (ulong_t)get_total_free_bytes(MALLOC_CAP_SPIRAM),
-        .total_free_bytes_internal   = (ulong_t)get_total_free_bytes(MALLOC_CAP_INTERNAL),
-        .total_free_bytes_default    = (ulong_t)get_total_free_bytes(MALLOC_CAP_DEFAULT),
-        .largest_free_block_exec     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_EXEC),
-        .largest_free_block_32bit    = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_32BIT),
-        .largest_free_block_8bit     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_8BIT),
-        .largest_free_block_dma      = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_DMA),
-        .largest_free_block_pid2     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID2),
-        .largest_free_block_pid3     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID3),
-        .largest_free_block_pid4     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID4),
-        .largest_free_block_pid5     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID5),
-        .largest_free_block_pid6     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID6),
-        .largest_free_block_pid7     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID7),
-        .largest_free_block_spiram   = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM),
-        .largest_free_block_internal = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL),
-        .largest_free_block_default  = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT),
+        .received_advertisements          = gw_metrics.received_advertisements,
+        .uptime_us                        = esp_timer_get_time(),
+        .total_free_bytes.size_exec       = (ulong_t)get_total_free_bytes(MALLOC_CAP_EXEC),
+        .total_free_bytes.size_32bit      = (ulong_t)get_total_free_bytes(MALLOC_CAP_32BIT),
+        .total_free_bytes.size_8bit       = (ulong_t)get_total_free_bytes(MALLOC_CAP_8BIT),
+        .total_free_bytes.size_dma        = (ulong_t)get_total_free_bytes(MALLOC_CAP_DMA),
+        .total_free_bytes.size_pid2       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID2),
+        .total_free_bytes.size_pid3       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID3),
+        .total_free_bytes.size_pid4       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID4),
+        .total_free_bytes.size_pid5       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID5),
+        .total_free_bytes.size_pid6       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID6),
+        .total_free_bytes.size_pid7       = (ulong_t)get_total_free_bytes(MALLOC_CAP_PID7),
+        .total_free_bytes.size_spiram     = (ulong_t)get_total_free_bytes(MALLOC_CAP_SPIRAM),
+        .total_free_bytes.size_internal   = (ulong_t)get_total_free_bytes(MALLOC_CAP_INTERNAL),
+        .total_free_bytes.size_default    = (ulong_t)get_total_free_bytes(MALLOC_CAP_DEFAULT),
+        .largest_free_block.size_exec     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_EXEC),
+        .largest_free_block.size_32bit    = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_32BIT),
+        .largest_free_block.size_8bit     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_8BIT),
+        .largest_free_block.size_dma      = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_DMA),
+        .largest_free_block.size_pid2     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID2),
+        .largest_free_block.size_pid3     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID3),
+        .largest_free_block.size_pid4     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID4),
+        .largest_free_block.size_pid5     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID5),
+        .largest_free_block.size_pid6     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID6),
+        .largest_free_block.size_pid7     = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_PID7),
+        .largest_free_block.size_spiram   = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_SPIRAM),
+        .largest_free_block.size_internal = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_INTERNAL),
+        .largest_free_block.size_default  = (ulong_t)heap_caps_get_largest_free_block(MALLOC_CAP_DEFAULT),
     };
     return metrics;
 }
@@ -106,55 +116,55 @@ metrics_print_total_free_bytes(str_buf_t *p_str_buf, const metrics_info_t *p_met
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_EXEC\"} %lu\n",
-        p_metrics->total_free_bytes_exec);
+        p_metrics->total_free_bytes.size_exec);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_32BIT\"} %lu\n",
-        p_metrics->total_free_bytes_32bit);
+        p_metrics->total_free_bytes.size_32bit);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_8BIT\"} %lu\n",
-        p_metrics->total_free_bytes_8bit);
+        p_metrics->total_free_bytes.size_8bit);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_DMA\"} %lu\n",
-        p_metrics->total_free_bytes_dma);
+        p_metrics->total_free_bytes.size_dma);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID2\"} %lu\n",
-        p_metrics->total_free_bytes_pid2);
+        p_metrics->total_free_bytes.size_pid2);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID3\"} %lu\n",
-        p_metrics->total_free_bytes_pid3);
+        p_metrics->total_free_bytes.size_pid3);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID4\"} %lu\n",
-        p_metrics->total_free_bytes_pid4);
+        p_metrics->total_free_bytes.size_pid4);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID5\"} %lu\n",
-        p_metrics->total_free_bytes_pid5);
+        p_metrics->total_free_bytes.size_pid5);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID6\"} %lu\n",
-        p_metrics->total_free_bytes_pid6);
+        p_metrics->total_free_bytes.size_pid6);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_PID7\"} %lu\n",
-        p_metrics->total_free_bytes_pid7);
+        p_metrics->total_free_bytes.size_pid7);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_SPIRAM\"} %lu\n",
-        p_metrics->total_free_bytes_spiram);
+        p_metrics->total_free_bytes.size_spiram);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_INTERNAL\"} %lu\n",
-        p_metrics->total_free_bytes_internal);
+        p_metrics->total_free_bytes.size_internal);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_free_bytes{capability=\"MALLOC_CAP_DEFAULT\"} %lu\n",
-        p_metrics->total_free_bytes_default);
+        p_metrics->total_free_bytes.size_default);
 }
 
 static void
@@ -163,55 +173,55 @@ metrics_print_largest_free_blk(str_buf_t *p_str_buf, const metrics_info_t *p_met
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_EXEC\"} %lu\n",
-        p_metrics->largest_free_block_exec);
+        p_metrics->largest_free_block.size_exec);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_32BIT\"} %lu\n",
-        p_metrics->largest_free_block_32bit);
+        p_metrics->largest_free_block.size_32bit);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_8BIT\"} %lu\n",
-        p_metrics->largest_free_block_8bit);
+        p_metrics->largest_free_block.size_8bit);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_DMA\"} %lu\n",
-        p_metrics->largest_free_block_dma);
+        p_metrics->largest_free_block.size_dma);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID2\"} %lu\n",
-        p_metrics->largest_free_block_pid2);
+        p_metrics->largest_free_block.size_pid2);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID3\"} %lu\n",
-        p_metrics->largest_free_block_pid3);
+        p_metrics->largest_free_block.size_pid3);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID4\"} %lu\n",
-        p_metrics->largest_free_block_pid4);
+        p_metrics->largest_free_block.size_pid4);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID5\"} %lu\n",
-        p_metrics->largest_free_block_pid5);
+        p_metrics->largest_free_block.size_pid5);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID6\"} %lu\n",
-        p_metrics->largest_free_block_pid6);
+        p_metrics->largest_free_block.size_pid6);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_PID7\"} %lu\n",
-        p_metrics->largest_free_block_pid7);
+        p_metrics->largest_free_block.size_pid7);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_SPIRAM\"} %lu\n",
-        p_metrics->largest_free_block_spiram);
+        p_metrics->largest_free_block.size_spiram);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_INTERNAL\"} %lu\n",
-        p_metrics->largest_free_block_internal);
+        p_metrics->largest_free_block.size_internal);
     str_buf_printf(
         p_str_buf,
         METRICS_PREFIX "heap_largest_free_block_bytes{capability=\"MALLOC_CAP_DEFAULT\"} %lu\n",
-        p_metrics->largest_free_block_default);
+        p_metrics->largest_free_block.size_default);
 }
 
 static void
