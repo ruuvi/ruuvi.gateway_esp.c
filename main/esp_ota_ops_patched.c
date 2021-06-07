1d124dd2b0613ccd5e08e4b1a06d78186f4d95dd
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2021-06-07 14:29:54 +0700

    #60: esp_ota_ops_patched: refactoring to fix critical code smells found by sonarcloud and reduce cognitive complexity

--- main/esp_ota_ops_patched.c
+++ main/esp_ota_ops_patched.c
@@ -42,7 +42,11 @@
 #include "esp_system.h"
 #include "esp_efuse.h"
 
-#define SUB_TYPE_ID(i) (i & 0x0F)
+extern esp_err_t
+erase_partition_with_sleep(const esp_partition_t *const p_partition);
+
+#define ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE (16U)
+#define ESP_OTA_FLASH_ENCRYPTION_FILL           (0xFFU)
 
 typedef struct ota_ops_entry_
 {
@@ -51,7 +55,7 @@ typedef struct ota_ops_entry_
     uint32_t               erased_size;
     uint32_t               wrote_size;
     uint8_t                partial_bytes;
-    uint8_t                partial_data[16];
+    uint8_t                partial_data[ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE];
     LIST_ENTRY(ota_ops_entry_) entries;
 } ota_ops_entry_t;
 
@@ -67,43 +71,40 @@ static bool
 is_ota_partition(const esp_partition_t *p)
 {
     return (
-        p != NULL && p->type == ESP_PARTITION_TYPE_APP && p->subtype >= ESP_PARTITION_SUBTYPE_APP_OTA_0
-        && p->subtype < ESP_PARTITION_SUBTYPE_APP_OTA_MAX);
+        (NULL != p) && (ESP_PARTITION_TYPE_APP == p->type) && (p->subtype >= ESP_PARTITION_SUBTYPE_APP_OTA_0)
+        && (p->subtype < ESP_PARTITION_SUBTYPE_APP_OTA_MAX));
 }
 
 esp_err_t
-esp_ota_begin_patched(const esp_partition_t *partition, esp_ota_handle_t *out_handle)
+esp_ota_begin_patched(const esp_partition_t *p_partition, esp_ota_handle_t *p_out_handle)
 {
-    ota_ops_entry_t *new_entry;
-    esp_err_t        ret = ESP_OK;
-
-    if ((partition == NULL) || (out_handle == NULL))
+    if ((NULL == p_partition) || (NULL == p_out_handle))
     {
         return ESP_ERR_INVALID_ARG;
     }
 
-    partition = esp_partition_verify(partition);
-    if (partition == NULL)
+    p_partition = esp_partition_verify(p_partition);
+    if (NULL == p_partition)
     {
         return ESP_ERR_NOT_FOUND;
     }
 
-    if (!is_ota_partition(partition))
+    if (!is_ota_partition(p_partition))
     {
         return ESP_ERR_INVALID_ARG;
     }
 
-    const esp_partition_t *running_partition = esp_ota_get_running_partition();
-    if (partition == running_partition)
+    const esp_partition_t *p_running_partition = esp_ota_get_running_partition();
+    if (p_partition == p_running_partition)
     {
         return ESP_ERR_OTA_PARTITION_CONFLICT;
     }
 
 #ifdef CONFIG_BOOTLOADER_APP_ROLLBACK_ENABLE
-    esp_ota_img_states_t ota_state_running_part;
-    if (esp_ota_get_state_partition(running_partition, &ota_state_running_part) == ESP_OK)
+    esp_ota_img_states_t ota_state_running_part = ESP_OTA_IMG_UNDEFINED;
+    if (esp_ota_get_state_partition(p_running_partition, &ota_state_running_part) == ESP_OK)
     {
-        if (ota_state_running_part == ESP_OTA_IMG_PENDING_VERIFY)
+        if (ESP_OTA_IMG_PENDING_VERIFY == ota_state_running_part)
         {
             ESP_LOGE(TAG, "Running app has not confirmed state (ESP_OTA_IMG_PENDING_VERIFY)");
             return ESP_ERR_OTA_ROLLBACK_INVALID_STATE;
@@ -111,100 +112,105 @@ esp_ota_begin_patched(const esp_partition_t *partition, esp_ota_handle_t *out_ha
     }
 #endif
 
-    extern esp_err_t erase_partition_with_sleep(const esp_partition_t *const p_partition);
-
-    ret = erase_partition_with_sleep(partition);
-
-    if (ret != ESP_OK)
+    const esp_err_t ret = erase_partition_with_sleep(p_partition);
+    if (ESP_OK != ret)
     {
         return ret;
     }
 
-    new_entry = (ota_ops_entry_t *)os_calloc(sizeof(ota_ops_entry_t), 1);
-    if (new_entry == NULL)
+    ota_ops_entry_t *const p_new_entry = (ota_ops_entry_t *)os_calloc(sizeof(ota_ops_entry_t), 1);
+    if (NULL == p_new_entry)
     {
         return ESP_ERR_NO_MEM;
     }
 
-    LIST_INSERT_HEAD(&s_ota_ops_entries_head, new_entry, entries);
+    LIST_INSERT_HEAD(&s_ota_ops_entries_head, p_new_entry, entries);
 
-    new_entry->erased_size = partition->size;
+    p_new_entry->erased_size = p_partition->size;
 
-    new_entry->part   = partition;
-    new_entry->handle = ++s_ota_ops_last_handle;
-    *out_handle       = new_entry->handle;
+    p_new_entry->part   = p_partition;
+    p_new_entry->handle = ++s_ota_ops_last_handle;
+    *p_out_handle       = p_new_entry->handle;
     return ESP_OK;
 }
 
-esp_err_t
-esp_ota_write_patched(esp_ota_handle_t handle, const void *data, size_t size)
+static esp_err_t
+esp_ota_write_entry(ota_ops_entry_t *const p_it, const void *const p_data, size_t size)
 {
-    const uint8_t *  data_bytes = (const uint8_t *)data;
-    esp_err_t        ret;
-    ota_ops_entry_t *it;
+    const uint8_t *p_data_bytes = (const uint8_t *)p_data;
 
-    if (data == NULL)
+    // must erase the partition before writing to p_it
+    assert((p_it->erased_size > 0) && "must erase the partition before writing to p_it");
+    if ((0 == p_it->wrote_size) && (0 == p_it->partial_bytes) && (size > 0)
+        && (ESP_IMAGE_HEADER_MAGIC != p_data_bytes[0]))
     {
-        ESP_LOGE(TAG, "write data is invalid");
+        ESP_LOGE(TAG, "OTA image has invalid magic byte (expected 0xE9, saw 0x%02x)", p_data_bytes[0]);
+        return ESP_ERR_OTA_VALIDATE_FAILED;
+    }
+
+    if (esp_flash_encryption_enabled())
+    {
+        /* Can only write 16 byte blocks to flash, so need to cache anything else */
+
+        /* check if we have partially written p_data from earlier */
+        if (0 != p_it->partial_bytes)
+        {
+            const size_t copy_len = MIN(ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE - p_it->partial_bytes, size);
+            memcpy(p_it->partial_data + p_it->partial_bytes, p_data_bytes, copy_len);
+            p_it->partial_bytes += copy_len;
+            if (ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE != p_it->partial_bytes)
+            {
+                return ESP_OK; /* nothing to write yet, just filling buffer */
+            }
+            /* write 16 byte to partition */
+            const esp_err_t ret = esp_partition_write(
+                p_it->part,
+                p_it->wrote_size,
+                p_it->partial_data,
+                ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE);
+            if (ESP_OK != ret)
+            {
+                return ret;
+            }
+            p_it->partial_bytes = 0;
+            memset(p_it->partial_data, ESP_OTA_FLASH_ENCRYPTION_FILL, ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE);
+            p_it->wrote_size += ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE;
+            p_data_bytes += copy_len;
+            size -= copy_len;
+        }
+
+        /* check if we need to save trailing p_data that we're about to write */
+        p_it->partial_bytes = size % ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE;
+        if (0 != p_it->partial_bytes)
+        {
+            size -= p_it->partial_bytes;
+            memcpy(p_it->partial_data, p_data_bytes + size, p_it->partial_bytes);
+        }
+    }
+
+    const esp_err_t ret = esp_partition_write(p_it->part, p_it->wrote_size, p_data_bytes, size);
+    if (ESP_OK == ret)
+    {
+        p_it->wrote_size += size;
+    }
+    return ret;
+}
+
+esp_err_t
+esp_ota_write_patched(esp_ota_handle_t handle, const void *p_data, size_t size)
+{
+    if (p_data == NULL)
+    {
+        ESP_LOGE(TAG, "write p_data is invalid");
         return ESP_ERR_INVALID_ARG;
     }
 
     // find ota handle in linked list
-    for (it = LIST_FIRST(&s_ota_ops_entries_head); it != NULL; it = LIST_NEXT(it, entries))
+    for (ota_ops_entry_t *p_it = LIST_FIRST(&s_ota_ops_entries_head); p_it != NULL; p_it = LIST_NEXT(p_it, entries))
     {
-        if (it->handle == handle)
+        if (p_it->handle == handle)
         {
-            // must erase the partition before writing to it
-            assert(it->erased_size > 0 && "must erase the partition before writing to it");
-            if (it->wrote_size == 0 && it->partial_bytes == 0 && size > 0 && data_bytes[0] != ESP_IMAGE_HEADER_MAGIC)
-            {
-                ESP_LOGE(TAG, "OTA image has invalid magic byte (expected 0xE9, saw 0x%02x)", data_bytes[0]);
-                return ESP_ERR_OTA_VALIDATE_FAILED;
-            }
-
-            if (esp_flash_encryption_enabled())
-            {
-                /* Can only write 16 byte blocks to flash, so need to cache anything else */
-                size_t copy_len;
-
-                /* check if we have partially written data from earlier */
-                if (it->partial_bytes != 0)
-                {
-                    copy_len = MIN(16 - it->partial_bytes, size);
-                    memcpy(it->partial_data + it->partial_bytes, data_bytes, copy_len);
-                    it->partial_bytes += copy_len;
-                    if (it->partial_bytes != 16)
-                    {
-                        return ESP_OK; /* nothing to write yet, just filling buffer */
-                    }
-                    /* write 16 byte to partition */
-                    ret = esp_partition_write(it->part, it->wrote_size, it->partial_data, 16);
-                    if (ret != ESP_OK)
-                    {
-                        return ret;
-                    }
-                    it->partial_bytes = 0;
-                    memset(it->partial_data, 0xFF, 16);
-                    it->wrote_size += 16;
-                    data_bytes += copy_len;
-                    size -= copy_len;
-                }
-
-                /* check if we need to save trailing data that we're about to write */
-                it->partial_bytes = size % 16;
-                if (it->partial_bytes != 0)
-                {
-                    size -= it->partial_bytes;
-                    memcpy(it->partial_data, data_bytes + size, it->partial_bytes);
-                }
-            }
-
-            ret = esp_partition_write(it->part, it->wrote_size, data_bytes, size);
-            if (ret == ESP_OK)
-            {
-                it->wrote_size += size;
-            }
-            return ret;
+            return esp_ota_write_entry(p_it, p_data, size);
         }
     }
 
@@ -216,50 +222,54 @@ esp_ota_write_patched(esp_ota_handle_t handle, const void *data, size_t size)
 esp_err_t
 esp_ota_end_patched(esp_ota_handle_t handle)
 {
-    ota_ops_entry_t *it;
-    esp_err_t        ret = ESP_OK;
+    ota_ops_entry_t *p_it = NULL;
+    esp_err_t        ret  = ESP_OK;
 
-    for (it = LIST_FIRST(&s_ota_ops_entries_head); it != NULL; it = LIST_NEXT(it, entries))
+    for (p_it = LIST_FIRST(&s_ota_ops_entries_head); p_it != NULL; p_it = LIST_NEXT(p_it, entries))
     {
-        if (it->handle == handle)
+        if (p_it->handle == handle)
         {
             break;
         }
     }
 
-    if (it == NULL)
+    if (NULL == p_it)
     {
         return ESP_ERR_NOT_FOUND;
     }
 
-    /* 'it' holds the ota_ops_entry_t for 'handle' */
+    /* 'p_it' holds the ota_ops_entry_t for 'handle' */
 
     // esp_ota_end() is only valid if some data was written to this handle
-    if ((it->erased_size == 0) || (it->wrote_size == 0))
+    if ((0 == p_it->erased_size) || (0 == p_it->wrote_size))
     {
         ret = ESP_ERR_INVALID_ARG;
         goto cleanup;
     }
 
-    if (it->partial_bytes > 0)
+    if (p_it->partial_bytes > 0)
     {
         /* Write out last 16 bytes, if necessary */
-        ret = esp_partition_write(it->part, it->wrote_size, it->partial_data, 16);
-        if (ret != ESP_OK)
+        ret = esp_partition_write(
+            p_it->part,
+            p_it->wrote_size,
+            p_it->partial_data,
+            ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE);
+        if (ESP_OK != ret)
         {
             ret = ESP_ERR_INVALID_STATE;
             goto cleanup;
         }
-        it->wrote_size += 16;
-        it->partial_bytes = 0;
+        p_it->wrote_size += ESP_OTA_FLASH_ENCRYPTION_MIN_CHUNK_SIZE;
+        p_it->partial_bytes = 0;
     }
 
-    esp_image_metadata_t      data;
     const esp_partition_pos_t part_pos = {
-        .offset = it->part->address,
-        .size   = it->part->size,
+        .offset = p_it->part->address,
+        .size   = p_it->part->size,
     };
 
+    esp_image_metadata_t data = { 0 };
     if (esp_image_verify(ESP_IMAGE_VERIFY, &part_pos, &data) != ESP_OK)
     {
         ret = ESP_ERR_OTA_VALIDATE_FAILED;
@@ -267,7 +277,7 @@ esp_ota_end_patched(esp_ota_handle_t handle)
     }
 
 cleanup:
-    LIST_REMOVE(it, entries);
-    os_free(it);
+    LIST_REMOVE(p_it, entries);
+    os_free(p_it);
     return ret;
 }
