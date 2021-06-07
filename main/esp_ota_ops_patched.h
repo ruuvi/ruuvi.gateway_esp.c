0d8a25eda9e3a0778ff83661f1bc2244ea39f01b
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2021-06-07 14:29:54 +0700

    #60: esp_ota_ops_patched: refactoring to fix critical code smells found by sonarcloud and reduce cognitive complexity

--- main/esp_ota_ops_patched.h
+++ main/esp_ota_ops_patched.h
@@ -18,10 +18,10 @@ extern "C" {
 #endif
 
 esp_err_t
-esp_ota_begin_patched(const esp_partition_t *partition, esp_ota_handle_t *out_handle);
+esp_ota_begin_patched(const esp_partition_t *p_partition, esp_ota_handle_t *p_out_handle);
 
 esp_err_t
-esp_ota_write_patched(esp_ota_handle_t handle, const void *data, size_t size);
+esp_ota_write_patched(esp_ota_handle_t handle, const void *p_data, size_t size);
 
 esp_err_t
 esp_ota_end_patched(esp_ota_handle_t handle);
