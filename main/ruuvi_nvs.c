16b64639199d3e55c971fff64a315e77f3874a01
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-05-07 14:53:30 +0300

    #515: Refactoring: ruuv_nvs.c: rename variables

--- main/ruuvi_nvs.c
+++ main/ruuvi_nvs.c
@@ -23,20 +23,21 @@ static const char TAG[] = "ruuvi_nvs";
 bool
 ruuvi_nvs_open(nvs_open_mode_t open_mode, nvs_handle_t *p_handle)
 {
-    const char *nvs_name = RUUVI_GATEWAY_NVS_NAMESPACE;
-    esp_err_t   err      = nvs_open(nvs_name, open_mode, p_handle);
+    const char *const p_nvs_name = RUUVI_GATEWAY_NVS_NAMESPACE;
+
+    esp_err_t err = nvs_open(p_nvs_name, open_mode, p_handle);
     if (ESP_OK != err)
     {
         if (ESP_ERR_NVS_NOT_INITIALIZED == err)
         {
-            LOG_WARN("NVS namespace '%s': StorageState is INVALID, need to erase NVS", nvs_name);
+            LOG_WARN("NVS namespace '%s': StorageState is INVALID, need to erase NVS", p_nvs_name);
             return false;
         }
         if (ESP_ERR_NVS_NOT_FOUND == err)
         {
-            LOG_WARN("NVS namespace '%s' doesn't exist and mode is NVS_READONLY, try to create it", nvs_name);
+            LOG_WARN("NVS namespace '%s' doesn't exist and mode is NVS_READONLY, try to create it", p_nvs_name);
             nvs_handle handle = 0;
-            err               = nvs_open(nvs_name, NVS_READWRITE, &handle);
+            err               = nvs_open(p_nvs_name, NVS_READWRITE, &handle);
             if (ESP_OK != err)
             {
                 LOG_ERR_ESP(err, "Can't open NVS for writing");
@@ -51,17 +52,17 @@ ruuvi_nvs_open(nvs_open_mode_t open_mode, nvs_handle_t *p_handle)
             }
             nvs_close(handle);
 
-            LOG_INFO("NVS namespace '%s' created successfully", nvs_name);
-            err = nvs_open(nvs_name, open_mode, p_handle);
+            LOG_INFO("NVS namespace '%s' created successfully", p_nvs_name);
+            err = nvs_open(p_nvs_name, open_mode, p_handle);
             if (ESP_OK != err)
             {
-                LOG_ERR_ESP(err, "Can't open NVS namespace: '%s'", nvs_name);
+                LOG_ERR_ESP(err, "Can't open NVS namespace: '%s'", p_nvs_name);
                 return false;
             }
         }
         else
         {
-            LOG_ERR_ESP(err, "Can't open NVS namespace: '%s'", nvs_name);
+            LOG_ERR_ESP(err, "Can't open NVS namespace: '%s'", p_nvs_name);
             return false;
         }
     }
