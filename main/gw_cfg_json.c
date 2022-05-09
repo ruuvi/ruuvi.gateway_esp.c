98887c93aac41d5dfe99ae26101a6589bb030714
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-05-09 21:15:27 +0300

    Fix #511: UI: Access Settings: Bearer authentication is always displayed as enabled (#512)
    * Fix #511: UI: Access Settings: Bearer authentication is always displayed as enabled
    
    * #511: Update submodule ruuvi.gwui.html to the master branch

--- main/gw_cfg_json.c
+++ main/gw_cfg_json.c
@@ -349,7 +349,17 @@ gw_cfg_json_add_items_lan_auth(
                 break;
         }
     }
-    if (!flag_hide_passwords)
+    if (flag_hide_passwords)
+    {
+        if (!gw_cfg_json_add_bool(
+                p_json_root,
+                "lan_auth_api_key_use",
+                ('\0' != p_cfg_lan_auth->lan_auth_api_key.buf[0]) ? true : false))
+        {
+            return false;
+        }
+    }
+    else
     {
         if (!gw_cfg_json_add_string(p_json_root, "lan_auth_api_key", p_cfg_lan_auth->lan_auth_api_key.buf))
         {
