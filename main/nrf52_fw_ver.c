9ccbfb4a7e00cf66daafeaa83692bfacc31c87b7
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2022-12-06 20:11:08 +0700

    #660: nrf52_fw_ver: Allow NULL argument in nrf52_fw_ver_get_str

--- main/nrf52_fw_ver.c
+++ main/nrf52_fw_ver.c
@@ -15,16 +15,19 @@
 #define BIT_IDX_OF_BYTE_0 (0U * 8U)
 #define BYTE_MASK         (0xFFU)
 
+#define NRF52_FW_VER_UNDEFINED (0xFFFFFFFFU)
+
 ruuvi_nrf52_fw_ver_str_t
 nrf52_fw_ver_get_str(const ruuvi_nrf52_fw_ver_t* const p_nrf52_fw_ver)
 {
     ruuvi_nrf52_fw_ver_str_t nrf52_fw_ver_str = { 0 };
-    snprintf(
+    const uint32_t           fw_ver = (NULL != p_nrf52_fw_ver) ? p_nrf52_fw_ver->version : NRF52_FW_VER_UNDEFINED;
+    (void)snprintf(
         nrf52_fw_ver_str.buf,
         sizeof(nrf52_fw_ver_str.buf),
         "v%u.%u.%u",
-        (printf_uint_t)((p_nrf52_fw_ver->version >> BIT_IDX_OF_BYTE_3) & BYTE_MASK),
-        (printf_uint_t)((p_nrf52_fw_ver->version >> BIT_IDX_OF_BYTE_2) & BYTE_MASK),
-        (printf_uint_t)((p_nrf52_fw_ver->version >> BIT_IDX_OF_BYTE_1) & BYTE_MASK));
+        (printf_uint_t)((fw_ver >> BIT_IDX_OF_BYTE_3) & BYTE_MASK),
+        (printf_uint_t)((fw_ver >> BIT_IDX_OF_BYTE_2) & BYTE_MASK),
+        (printf_uint_t)((fw_ver >> BIT_IDX_OF_BYTE_1) & BYTE_MASK));
     return nrf52_fw_ver_str;
 }
