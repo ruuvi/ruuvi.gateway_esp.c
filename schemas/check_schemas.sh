347ad21f891571ea844a9dc83ed3007a190322a9
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-02-24 21:37:38 +0700

    #533: Update documentation, add json schemas for data formats used (#703)
    * #533: Add json schema for data formats used by Ruuvi Gateway
    
    * #533: Update json schemas for all data formats, add json examples

---
+++ schemas/check_schemas.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://check-jsonschema.readthedocs.io/en/latest/
+# https://github.com/python-jsonschema/check-jsonschema
+# pip install check-jsonschema
+
+set -e -x
+
+check-jsonschema --schemafile ./ruuvi_gw_cfg.schema.json ../gw_cfg_default/gw_cfg_default.json
+
+check-jsonschema --schemafile ./ruuvi_gw_status.schema.json ./examples/gw_status_reset_reason_panic.json
+
+check-jsonschema --schemafile ./ruuvi_history.schema.json ./examples/history.json
+
+check-jsonschema --schemafile ./ruuvi_http_data_with_timestamps.schema.json ./examples/http_data_with_timestamps.json
+
+check-jsonschema --schemafile ./ruuvi_http_data_without_timestamps.schema.json ./examples/http_data_without_timestamps.json
+
+check-jsonschema --schemafile ./ruuvi_mqtt_data_with_timestamps.schema.json ./examples/mqtt_data_with_timestamps.json
+
+check-jsonschema --schemafile ./ruuvi_mqtt_data_without_timestamps.schema.json ./examples/mqtt_data_without_timestamps.json
