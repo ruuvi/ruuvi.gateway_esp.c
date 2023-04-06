854de2e44c519f7eb1e3436dee22884ce3cc5d27
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-04-06 16:37:39 +0700

    #728: runtime_stat: Collect information on the minimum free stack space for each task

--- main/runtime_stat.h
+++ main/runtime_stat.h
@@ -8,12 +8,20 @@
 #ifndef RUUVI_RUNTIME_STAT_H
 #define RUUVI_RUNTIME_STAT_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void
 log_runtime_statistics(void);
+
+bool
+runtime_stat_for_each_accumulated_info(
+    bool (*p_cb)(const char* const p_task_name, const uint32_t min_free_stack_size, void* p_userdata),
+    void* p_userdata);
 
 #ifdef __cplusplus
 }
