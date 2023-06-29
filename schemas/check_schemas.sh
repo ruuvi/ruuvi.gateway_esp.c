cefc838dc3105c670f67361dec378515bfb6e001
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-06-29 13:56:53 +0700

    #755: Update json schemas and examples

--- schemas/check_schemas.sh
+++ schemas/check_schemas.sh
@@ -6,18 +6,38 @@
 
 set -e -x
 
-check-jsonschema --schemafile ./ruuvi_gw_cfg.schema.json ../gw_cfg_default/gw_cfg_default.json
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-check-jsonschema --schemafile ./ruuvi_gw_status.schema.json ./examples/gw_status_after_reboot.json
+SCHEMAS_DIR=$SCRIPT_DIR
+SCHEMA_RUUVI_GW_CFG=$SCHEMAS_DIR/ruuvi_gw_cfg.schema.json
+EXAMPLES=$SCRIPT_DIR/examples
 
-check-jsonschema --schemafile ./ruuvi_gw_status.schema.json ./examples/gw_status_reset_reason_panic.json
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$SCRIPT_DIR/../gw_cfg_default/gw_cfg_default.json"
 
-check-jsonschema --schemafile ./ruuvi_history.schema.json ./examples/history.json
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_remote_cfg_auth_none.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_remote_cfg_auth_no.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_remote_cfg_auth_basic.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_remote_cfg_auth_bearer.json"
 
-check-jsonschema --schemafile ./ruuvi_http_data_with_timestamps.schema.json ./examples/http_data_with_timestamps.json
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_http_auth_none.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_http_auth_basic.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_http_auth_bearer.json"
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/gw_cfg_http_auth_token.json"
 
-check-jsonschema --schemafile ./ruuvi_http_data_without_timestamps.schema.json ./examples/http_data_without_timestamps.json
+check-jsonschema --schemafile "$SCHEMA_RUUVI_GW_CFG" "$EXAMPLES/ruuvi_1.json"
 
-check-jsonschema --schemafile ./ruuvi_mqtt_data_with_timestamps.schema.json ./examples/mqtt_data_with_timestamps.json
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_gw_status.schema.json" "$EXAMPLES/gw_status_after_reboot.json"
 
-check-jsonschema --schemafile ./ruuvi_mqtt_data_without_timestamps.schema.json ./examples/mqtt_data_without_timestamps.json
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_gw_status.schema.json" "$EXAMPLES/gw_status_reset_reason_panic.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_history.schema.json" "$EXAMPLES/history.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_history.schema.json" "$EXAMPLES/history_with_decoding_df5.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_http_data_with_timestamps.schema.json" "$EXAMPLES/http_data_with_timestamps.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_http_data_without_timestamps.schema.json" "$EXAMPLES/http_data_without_timestamps.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_mqtt_data_with_timestamps.schema.json" "$EXAMPLES/mqtt_data_with_timestamps.json"
+
+check-jsonschema --schemafile "$SCHEMAS_DIR/ruuvi_mqtt_data_without_timestamps.schema.json" "$EXAMPLES/mqtt_data_without_timestamps.json"
