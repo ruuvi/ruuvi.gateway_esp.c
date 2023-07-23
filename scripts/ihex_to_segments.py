2a443ccabecd327565e78f226f5b846cf7bfb205
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-07-23 20:38:08 +0700

    Configure GitHub actions (#822)
    * Configure GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update README.md to restart build
    
    * Revert "Update README.md to restart build"
    
    This reverts commit 2ff84241de8e960ec0a918c1cfefb20c71f813d9.
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub actions
    
    * Update GitHub Actions
    
    * Update submodules to the master branch
    
    * Remove deprecated scripts

--- scripts/ihex_to_segments.py
+++ scripts/ihex_to_segments.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 # @file ihex_to_segments.py
 # @author TheSomeMan
---
+++ scripts/sonar_scan.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e # Exit script on non-zero command exit status
+set -x # Print commands and their arguments as they are executed.
+
+export SONAR_TOKEN=$SONAR_TOKEN_ruuvi_gateway_esp
+if [ "$SONAR_TOKEN" = "" ]; then
+     echo Environment variable "SONAR_TOKEN_ruuvi_gateway_esp" is not set
+     exit 1
+fi
+
+mkdir -p "$HOME/bin"
+if [ ! -d "$HOME/bin/sonar-scanner" ]; then
+  rm -f "$HOME/bin/sonar-scanner-cli-4.7.0.2747-linux.zip"
+  wget -P "$HOME/bin" "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip"
+  unzip -q -o "$HOME/bin/sonar-scanner-cli-4.7.0.2747-linux.zip" -d "$HOME/bin"
+  mv "$HOME/bin/sonar-scanner-4.7.0.2747-linux" "$HOME/bin/sonar-scanner"
+fi
+export PATH=$PATH:$HOME/bin/sonar-scanner/bin
+
+rm -f "$HOME/bin/build-wrapper-linux-x86.zip"
+wget -P "$HOME/bin" https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -o "$HOME/bin/build-wrapper-linux-x86.zip" -d "$HOME/bin"
+export PATH=$PATH:$HOME/bin/build-wrapper-linux-x86
+
+echo Initial cleanup
+find . -type f -name "*.gcno" -exec rm -f {} \;
+find . -type f -name '*.gcna' -exec rm -f {} \;
+find . -type f -name "*.gcov" -exec rm -f {} \;
+find . -type f -name 'gtestresults.xml' -exec rm -f {} \;
+rm -rf build
+mkdir -p build
+cd build
+which build-wrapper-linux-x86-64
+build-wrapper-linux-x86-64 --out-dir ./bw-output cmake -DCMAKE_BUILD_TYPE=Debug ..
+build-wrapper-linux-x86-64 --out-dir ./bw-output make -j $(nproc)
+cd ..
+
+rm -f coverage.xml
+cd tests
+rm -rf build
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="--coverage" ..
+make -j $(nproc)
+ctest
+cd ../..
+gcovr -r . --sonarqube -o coverage.xml
+
+which sonar-scanner
+sonar-scanner --version
+sonar-scanner -X \
+  --define sonar.cfamily.build-wrapper-output="build/bw-output" \
+  --define sonar.coverageReportPaths=coverage.xml \
+  --define sonar.host.url=https://sonarcloud.io \
+  --define sonar.cfamily.threads=$(nproc)
+
+echo Final cleanup
+rm -f coverage.xml
+rm -rf build
+rm -rf tests/build
+find . -type f -name "*.gcno" -exec rm -f {} \;
+find . -type f -name '*.gcna' -exec rm -f {} \;
+find . -type f -name "*.gcov" -exec rm -f {} \;
+find . -type f -name 'gtestresults.xml' -exec rm -f {} \;
