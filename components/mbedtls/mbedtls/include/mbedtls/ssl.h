b8f0fd1685649cae11211badefab30bcee91dcef
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2026-02-11 02:56:29 +0700

    [#1149] mbedtls_ssl_get_hostname: return NULL for empty hostname
    Updates mbedtls_ssl_get_hostname to return NULL when the hostname is empty instead of returning a pointer to an empty string.
    This change ensures correct handling of empty hostnames and avoids potential issues in applications relying on the return value of mbedtls_ssl_get_hostname.
    
    Fixes issue where tests were checking the length of an empty string instead of checking for NULL.

--- components/mbedtls/mbedtls/include/mbedtls/ssl.h
+++ components/mbedtls/mbedtls/include/mbedtls/ssl.h
@@ -3829,7 +3829,7 @@ int mbedtls_ssl_set_hostname(mbedtls_ssl_context *ssl, const char *hostname);
  */
 static inline const char *mbedtls_ssl_get_hostname(mbedtls_ssl_context *ssl)
 {
-    return ssl->MBEDTLS_PRIVATE(hostname).buf;
+    return ('\0' != ssl->MBEDTLS_PRIVATE(hostname).buf[0]) ? ssl->MBEDTLS_PRIVATE(hostname).buf : NULL;
 }
 #endif /* MBEDTLS_X509_CRT_PARSE_C */
 
